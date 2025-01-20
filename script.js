79de25eee193eda7c528eea7e6f3d17c647bfdfa
talha-hyphen <w00052207@gmail.com>
2025-01-20 10:48:26 -0800

    programs

---
+++ script.js
@@ -0,0 +1,51 @@
+//14-Write a js Program to
+
+
+
+
+
+// //13-Write a js Program to reverse order of words in a string.
+// var reverse = ["a","b","c","d"] 
+// console.log(reverse.reverse())
+
+
+
+
+
+// //1-Write a js Program to find length a of string.
+// var findLenght = "12ab$$'uyds\\'"
+// console.log(findLenght.length)
+
+
+
+
+
+// //8-Write a js Program to find number of alphabets,digits or special character in a  string.
+// var findlenght = "12ab$$'uyds\\'"
+// console.log(findlenght.length)
+
+
+
+
+
+// //5-Write a js program to convert lowercase string to uppercase.
+// var lowtoUpper = "jguytl45yylslf"
+//  console.log( lowtoUpper.toUpperCase())
+
+
+
+
+// //6-Write a js program to convert uppercase string to lowercase.
+ //var Uppertolow = "jguyYTTYDDKTtl45yylslf"
+//  console.log( Uppertolow.toLowerCase())
+
+//  //Write a js program to concat two strings
+//  var  string1 = "1"
+//  var string2 = "2"
+//  var string3 = "3"
+//  var string4="4"
+//   console.log(string1.concat(string2,string3,string4) )
+
+
+
+
